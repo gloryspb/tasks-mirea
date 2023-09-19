34beb4bbfeb8386fce3e06cf4c6e276544f3d156
gloryspb <spbbglory@gmail.com>
2023-09-19 13:22:20 +0300

    initial commit

---
+++ tt1/tt1/tt1.cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream>
+#include <typeinfo>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+void Task1(string name)
+{
+    cout << name << endl;
+}
+
+string Task2(double num1, double num2)
+{
+    string str = to_string(num1 + num2) + " " + to_string(num1 - num2) + " " + to_string(num1 * num2);
+    if (num2 != 0)
+        str += " " + to_string(num1 / num2);
+    return str;
+}
+
+string Task3(double b, double c)
+{
+    if (c == 0 && b == 0)
+    {
+        return "endless solutions";
+    }
+    else if (c == 0)
+    {
+        return "0";
+    }
+    else if (b == 0)
+    {
+        return "no solution";
+    }
+    else
+    {
+        return to_string(-c / b);
+    }
+    // return b != 0 ? to_string(-c / b) : "no solution";
+}
+
+string Task4(double a, double b, double c)
+{
+    if (a == 0)
+        return Task3(b, c);
+
+    double d = b * b - 4 * a * c;
+
+    if (d < 0) return "no solution";
+
+    double x1, x2;
+    x1 = (-b + sqrt(d)) / (2 * a);
+    x2 = (-b - sqrt(d)) / (2 * a);
+
+    if (d == 0)
+    {
+        return to_string(x1);
+    }
+
+    return to_string(x1) + " " + to_string(x2);
+}
+
+bool Task5(bool isDay, bool isCurtainsOpened, bool isLampOn)
+{
+    return isDay && isCurtainsOpened || isLampOn;
+}
+
+int main()
+{
+    Task1("Dima");
+    Task1("Kirill");
+    // ввод с клавиатуры
+    double num1, num2;
+    cin >> num1;
+    cin >> num2;
+    cout << Task2(num1, num2) << endl;
+
+    double a, b, c;
+    cin >> b;
+    cin >> c;
+    cout << Task3(b, c) << endl;
+
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cout << Task4(a, b, c) << endl;
+
+    if (Task5(true, false, true))
+    {
+        cout << "svetlo" << endl;
+    }
+    else
+    {
+        cout << "temno" << endl;
+    }
+    // cout << Task5(true, false, true) ? "svetlo" : "temno" << endl;
+
+    return 0;
+}
---
+++ tt2/tt2/tt2.cpp
@@ -0,0 +1,136 @@
+﻿#include <iostream>
+#include <math.h>
+#include <string>
+
+using namespace std;
+
+class Cone
+{
+private:
+	double height,
+		   radiusBase,
+		   radiusVertex;
+
+public:
+	Cone(double height, double radiusBase, double radiusVertex);
+
+	double GetVolume();
+	double GetArea();
+};
+
+
+Cone::Cone(double _height, double _radiusBase, double _radiusVertex)
+{
+	height = _height;
+	radiusBase = _radiusBase;
+	radiusVertex = _radiusVertex;
+}
+
+double Cone::GetVolume()
+{
+	return 1.0/3 * 3.14 * height * (radiusBase * radiusBase + radiusVertex * radiusBase + radiusVertex * radiusVertex);
+}
+
+double Cone::GetArea()
+{
+	return 3.14 * (radiusBase * radiusBase + (radiusVertex * radiusBase)
+		* (sqrt(radiusBase * radiusBase + height * height))
+		+ radiusVertex * radiusVertex);
+}
+
+
+void Task1()
+{
+	cout << "Enter the height, base radius and top radius:" << endl;
+	double _height, 
+		   _radiusBase, 
+		   _radiusVertex;
+	cin >> _height >> _radiusBase >> _radiusVertex;
+
+	//Cone _cone = new Cone(_height, _radiusBase, _radiusVertex);
+
+	Cone _cone(_height, _radiusBase, _radiusVertex);
+
+	cout << _cone.GetArea() << endl;
+	cout << _cone.GetVolume() << endl;
+}
+
+double Task2(double numA, double numX)
+{
+	if (abs(numX) < 1)
+	{
+		return numA * log(abs(numX));
+	}
+	else
+	{
+		double num = numA - numX * numX;
+
+		return num >= 0 ? sqrt(num) : NULL;
+	}
+}
+
+double Task3(double b,  double x, double y)
+{
+	if (b - x < 0 || b - y < 0)
+	{
+		cout << "error!" << endl;
+		return 0;
+	}
+
+	return log(b - y) * sqrt(b - x);
+}
+
+void Task4(int num)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		cout << num << endl;
+		num++;
+	}
+}
+
+void Task5()
+{
+	for (double x = -4; x <= 4; x += 0.5)
+	{
+		cout << "x = " + to_string(x) + ", y = " + to_string((x * x - 2 * x + 2) / (x - 1));
+	}
+}
+
+int main()
+{
+	// задание 1
+
+	Task1();
+	
+	// задание 2
+
+	double numA, numX;
+	cout << "Enter 2 nums" << endl;
+	cin >> numA >> numX;
+	cout << Task2(numA, numX) << endl;
+
+	// задание 3
+	double b, x, y;
+
+	cout << "Enter function parameters (b, x, y):" << endl;
+
+	cin >> b >> x >> y;
+
+	cout << Task3(b, x, y) << endl;
+
+
+	// задание 4
+	double num;
+
+	cout << "Enter the number:" << endl;
+	cin >> num;
+	Task4(num);
+
+	// задание 5
+
+	cout << "Tabulation:" << endl;
+	Task5();
+
+	return 0;
+}
